624767f3b8348666486d2a64653cded949eaaa06
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-04 11:16:04 +0330

    add description about __new__ vs __init__

--- section-01.py
+++ section-01.py
@@ -1,1 +1,7 @@
 # In this section we're going over __new__ function
+
+# Whenever a new object of a class is instantiated __new__ and __init__ methods are called.
+# __new__ method will be called when an object is created and __init__ method will be called to initialize the object.
+# So in a nutshell:
+# __new__ -> for creating object
+# __init__ -> for initialize object (setting values)
